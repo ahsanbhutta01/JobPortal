d54754ce7c9b90007dac76967b8d7596bb034d0c
Ahsan Gulzar <162182753+ahsanbhutta01@users.noreply.github.com>
2024-08-23 12:00:56 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,29 +7,29 @@ import userRoute from './routes/user.route.js';
 import companyRoute from './routes/company.route.js';
 import jobRoute from './routes/job.route.js';
 import applicationRoute from './routes/application.route.js';
+
 const app = express();
 dotenv.config();
 
-
-//middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(cors({
-   origin: ['https://job-portal-frontend-inky.vercel.app/'],
+   origin: ['https://job-portal-frontend-inky.vercel.app', 'https://job-portal-backend-ten-rho.vercel.app'],
    credentials: true
 }));
+
 app.use(cookieParser());
-//api's
+
+// API routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
    connectDB();
-   console.log(`Server is running on ${PORT} `)
-})
+   console.log(`Server is running on ${PORT}`);
+});
